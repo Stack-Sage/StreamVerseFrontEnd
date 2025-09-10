fdd818c59cc56657aac807fccdb22a99de8ebc52
Stack-Sage <adarsh.pathania.04@gmail.com>
2025-09-10 19:38:29 +0530

    video uplaod form layout improved

--- src/app/videos/page.js
+++ src/app/videos/page.js
@@ -4,21 +4,19 @@ import VideoUploadForm from "@/components/Video/VideoUploadForm";
 
 export default function Videos() {
   return (
-    <main className="mb-20 relative mx-auto items-center justify-center flex rounded-lg min-h-screen pt-16 ">
+    <main className="mb-20  flex-col relative mx-auto items-center justify-center flex rounded-lg min-h-screen pt-16 ">
       
       
-      <div className="pt-8 text-center max-w-2xl mx-auto">
-        <p className="text-neutral-700 dark:text-neutral-300 mb-4">
+      <div className="pt-4 text-center max-w-2xl mx-auto">
+        <p className="text-neutral-700 text-lg dark:text-neutral-300 mb-4">
           Here you can <strong>explore videos, like, comment</strong>, and upload your own videos .
+         <VideoUploadForm/>
+             
         </p>
-        <p className="text-sm text-neutral-500 dark:text-neutral-400">
+        <p className="text-md text-neutral-500 dark:text-neutral-400">
           Note: Currently all the videos are uploaded by my friends or test accounts since this app is not yet in production. 
         </p>
       </div>
-      <div className=' fixed lg:bottom-5 top-10 right-5 z-50'>
-      
-             <VideoUploadForm/>
-              </div>
 
  
       <div className="pt-12"> 
