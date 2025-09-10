e59b310b308597ae03c11dbb63ed6efdf2a7eb8c
Stack-Sage <adarsh.pathania.04@gmail.com>
2025-09-10 18:50:52 +0530

    env changed

--- src/components/Home/ExploreSection.jsx
+++ src/components/Home/ExploreSection.jsx
@@ -5,15 +5,15 @@ import { FaVideo, FaListUl, FaFire, FaUserPlus, FaUser } from 'react-icons/fa';
 const links = [
 
   { href: '/login', label: 'Log in', color: 'bg-blue-600 hover:bg-blue-700 ring-blue-400', icon: <FaUser /> },
-  { href: '/register', label: 'Sign Up', color: 'bg-indigo hover:bg-indigo-600 ring-indigo-400', icon: <FaUserPlus /> },
+  { href: '/register', label: 'Sign Up', color: 'bg-indigo-700 hover:bg-indigo-700 ring-indigo-400', icon: <FaUserPlus /> },
 ];
 
 export default function ExploreSection() {
   return (
     <div className="w-full max-w-6xl mb-16 px-2">
-      <span className='flex flex-wrap '>
-
       <h2 className="text-3xl font-extrabold text-neutral-900 dark:text-white mb-8 tracking-tight text-center">Explore</h2>
+      <span className='flex flex-wrap text-center items-center'>
+
       <h2 className=' text-neutral-900 dark:text-white mb-8 tracking-tight text-center '>If You Don't want to register and explore the Website, You can use this <p className='font-bold'>Demo Account</p> to login - Click on Login button and Fill up those Detials </h2>
       </span>
       <h3 className='text-blue-900 dark:text-blue-200 mb-8 tracking-tight text-center'> Username -  guest123 </h3>
